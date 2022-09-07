d2ebac3da1c026e35e3476a21005a2eda7c64908
Auvsexvc <auvsexvc@gmail.com>
2022-09-07 11:50:49 +0200

    PrepareViewModel further refactor

--- ExchangeRatesDownloaderApp/Data/DataProcessor.cs
+++ ExchangeRatesDownloaderApp/Data/DataProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using ExchangeRatesDownloaderApp.Interfaces;
 using ExchangeRatesDownloaderApp.Models;
 using System.Net;
+using System.Linq;
 
 namespace ExchangeRatesDownloaderApp.Data
 {
@@ -58,34 +59,40 @@ namespace ExchangeRatesDownloaderApp.Data
 
         private IEnumerable<ExchangeRateVM> PrepareViewModel(IEnumerable<ExchangeTable> data)
         {
-            var objC = data.Where(t => _nbpTablesBidAsk.Select(x => x.ToLower()).Contains(t.Type.ToLower())).ToList();
+            var bidAskTables = data
+                .Where(t => _nbpTablesBidAsk
+                    .Select(x => x.ToLower())
+                    .Contains(t.Type.ToLower()))
+                .ToList();
+
+            var midTables = data
+                .Where(t => _nbpTablesMid
+                    .Select(x => x.ToLower())
+                    .Contains(t.Type.ToLower()));
 
             List<ExchangeRateVM> exchangeRates = new();
 
-            foreach (var table in data.Where(t => _nbpTablesMid.Select(x => x.ToLower()).Contains(t.Type.ToLower())))
+            foreach (var (midTable, rate) in midTables.SelectMany(midTable => midTable.Rates.Select(rate => (midTable, rate))))
             {
-                foreach (var rate in table.Rates)
-                {
-                    (int index, bool hasBidAsk) = Enumerable
-                        .Range(0, objC.Count)
-                        .Select(x => (x, hasBidAsk: objC[x].Rates.Any(r => r.Code == rate.Code)))
-                        .FirstOrDefault(x => x.hasBidAsk);
+                (int index, bool currencyHasBidAsk) = Enumerable
+                                    .Range(0, bidAskTables.Count)
+                                    .Select(x => (x, hasBidAsk: bidAskTables[x].Rates.Any(r => r.Code == rate.Code)))
+                                    .FirstOrDefault(x => x.hasBidAsk);
 
-                    ExchangeRateVM exchangeRate = new()
-                    {
-                        Name = rate.Name,
-                        Code = rate.Code,
-                        Bid = hasBidAsk ? objC[index].Rates.Where(r => r.Code == rate.Code).Select(r => r.Bid).First() : rate.Bid,
-                        Ask = hasBidAsk ? objC[index].Rates.Where(r => r.Code == rate.Code).Select(r => r.Ask).First() : rate.Ask,
-                        Mid = rate.Mid,
-                        No = hasBidAsk ? String.Join("\n", table.No, objC[index].No) : table.No,
-                        Type = hasBidAsk ? String.Join("\n", table.Type, objC[index].Type) : table.Type,
-                        TradingDate = hasBidAsk ? objC[index].TradingDate : table.TradingDate,
-                        EffectiveDate = table.EffectiveDate,
-                        EffectiveDateBidAsk = hasBidAsk ? objC[index].EffectiveDate : null
-                    };
-                    exchangeRates.Add(exchangeRate);
-                }
+                ExchangeRateVM exchangeRate = new()
+                {
+                    Name = rate.Name,
+                    Code = rate.Code,
+                    Bid = currencyHasBidAsk ? bidAskTables[index].Rates.Where(r => r.Code == rate.Code).Select(r => r.Bid).First() : rate.Bid,
+                    Ask = currencyHasBidAsk ? bidAskTables[index].Rates.Where(r => r.Code == rate.Code).Select(r => r.Ask).First() : rate.Ask,
+                    Mid = rate.Mid,
+                    No = currencyHasBidAsk ? String.Join("\n", midTable.No, bidAskTables[index].No) : midTable.No,
+                    Type = currencyHasBidAsk ? String.Join("\n", midTable.Type, bidAskTables[index].Type) : midTable.Type,
+                    TradingDate = currencyHasBidAsk ? bidAskTables[index].TradingDate : midTable.TradingDate,
+                    EffectiveDate = midTable.EffectiveDate,
+                    EffectiveDateBidAsk = currencyHasBidAsk ? bidAskTables[index].EffectiveDate : null
+                };
+                exchangeRates.Add(exchangeRate);
             }
 
             return exchangeRates;
